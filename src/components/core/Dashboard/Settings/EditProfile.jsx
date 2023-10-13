749564a3e902131ffb79db3731c0624649186f2e
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-10-13 14:41:24 +0530

    change profile picture functionality

--- src/components/core/Dashboard/Settings/EditProfile.jsx
+++ src/components/core/Dashboard/Settings/EditProfile.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {useForm} from "react-hook-form"
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import {updatedProfile} from "../../../../services/operations/profileAPI"
+import IconBtn from '../../../common/IconBtn'
+
+const genders = ["Male", "Female", "Non-Binary", "Prefer not to say", "Othere"]
 
 const EditProfile = () => {
   return (
-    <div>EditProfile</div>
+    <div>
+
+    </div>
   )
 }
 
