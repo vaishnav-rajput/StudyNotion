2107696f3d2327e34fdd43432aef51e8c7c439f9
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-11-04 05:18:13 +0530

    handlers for prev and next video

--- src/components/core/ViewCourse/VideoDetails.jsx
+++ src/components/core/ViewCourse/VideoDetails.jsx
@@ -1,3 +1,4 @@
+import { current } from '@reduxjs/toolkit'
 import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
@@ -37,23 +38,80 @@ const VideoDetails = () => {
 
 
   const isFirstVideo = () => {
+    const currentSectionIndex = courseSectionData.findIndex((data) => data._id === sectionId)
     
+    const currentSubSectionIndex = courseSectionData[currentSectionIndex].subSection.findIndex((data) => data._id === subSectionId) 
+    if(currentSectionIndex === 0 && currentSubSectionIndex === 0){
+      return true
+    }
+    else {
+      return false    
+    }
   }
 
   const isLastVideo = () => {
+    const currentSectionIndex = courseSectionData.findIndex((data) => data._id === sectionId)
+    const noOfSubSections = courseSectionData[currentSectionIndex].subSection.length
+
+    const currentSubSectionIndex = courseSectionData[currentSectionIndex].subSection.findIndex((data) => data._id === subSectionId) 
+
+    if(currentSectionIndex === courseSectionData.length - 1 && currentSubSectionIndex === noOfSubSections - 1){
+      return true
+    }
+    else {
+      return false
+    }
 
   }
 
   const goToNextVideo = () => {
+    const currentSectionIndex = courseSectionData.findIndex((data) => data._id === sectionId)
+    const noOfSubSections = courseSectionData[currentSectionIndex].subSection.length
+     
+    const currentSubSectionIndex = courseSectionData[currentSectionIndex].subSection.findIndex((data) => data._id === subSectionId)
 
+    if(currentSubSectionIndex != noOfSubSections - 1){
+      //go to the next video in the same section 
+      const nextSubSectionId = courseSectionData[currentSectionIndex].subSection[currentSubSectionIndex + 1]._id 
+
+      //go to that video
+      navigate(`/view-course/${courseId}/section/${sectionId}/sub-section/${nextSubSectionId}`)
+
+    } else {
+      //different section's first video
+      const nextSectionId = courseSectionData[currentSectionIndex + 1]._id
+      const nextSubSectionId = courseSectionData[currentSectionIndex + 1].subSection[0]._id
+      //go to this video
+      navigate(`/view-course/${courseId}/section/${nextSectionId}/sub-section/${nextSubSectionId}`)
+    }
   }
 
   const goToPrevVideo = () => {
+    const currentSectionIndex = courseSectionData.findIndex((data) => data._id === sectionId)
+    const noOfSubSections = courseSectionData[currentSectionIndex].subSection.length
+     
+    const currentSubSectionIndex = courseSectionData[currentSectionIndex].subSection.findIndex((data) => data._id === subSectionId)
 
+    if(currentSubSectionIndex != 0){
+       //same section prev video
+       const prevSubSectionId = courseSectionData[currentSectionIndex].subSection[currentSubSectionIndex - 1]._id
+       //go to this video
+       navigate(`/view-course/${courseId}/section/${sectionId}/sub-section/${prevSubSectionId}`)
+
+
+    }else {
+      //different section last video
+      const prevSectionId = courseSectionData[currentSectionIndex - 1]._id
+      const prevSubSectionLength = courseSectionData[currentSectionIndex - 1].subSection.length
+      const prevSubSectionId = courseSectionData[currentSectionIndex - 1].subSection[prevSubSectionLength - 1]._id
+
+       //go to this video
+       navigate(`/view-course/${courseId}/section/${prevSectionId}/sub-section/${prevSubSectionId}`)
+    }
   }
 
   const handleLectureCompletion = () => {
-
+    //dummy code
   }
 
 
