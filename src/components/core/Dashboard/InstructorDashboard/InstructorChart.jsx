a10435b966f92f837a18755f9fe6ea6848efad24
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2023-11-06 05:18:45 +0530

    chart data for students

--- src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
+++ src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { useState } from 'react'
+import {Chart, registrables} from "chart.js"
+import {Pie} from "react-chartjs-2"
+import CoursesTable from '../InstructorCourses/CoursesTable'
 
-const InstructorChart = () => {
+Chart.register(...registrables)
+
+const InstructorChart = ({courses}) => {
+
+    const [currChart , setCurrChart] = useState("students")
+
+    //function to generatee random colors
+    const getRandomColors = (numColors) => {
+        const colors = []
+        for(let i=0; i<numColors; i++ ){
+            const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`
+            colors.push(color)
+        }
+        return colors
+    }
+
+    //create data for chart displaying student info
+    const chartDataForStudents = {
+        labels: courses.map((course) => course.courseName),
+        datasets: [
+            {
+                data: courses.map((course) => course.totalStudentsEnrolled ),
+                backgroundColor: getRandomColors(courses.length) 
+            },
+        ]
+    }
+
+    //create data for chart displaying income info
+    const chartDataForIncome = {
+        labels: courses.map((course) => course.courseName)
+    }
+
+    //create options 
+
+
   return (
-    <div>InstructorChart</div>
+    <div>
+
+    </div>
   )
 }
 
